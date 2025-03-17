e25a65d33fab9754f8f58b0f732db99768a13746
PhacharaChangyouang <Potbunpal@gmail.com>
2025-03-18 01:06:56 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,86 +1,19 @@
-const form = document.querySelector("form");
-const todoInput = document.querySelector("#todo-input");
-const addButton = document.querySelector("#add-button");
-const todoList = document.querySelector("#todo-list");
+const X_IMAGE_URL = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1083533/x.png';
+const O_IMAGE_URL = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1083533/circle.png';
+// Add event listeners?
 
-let todos = [];
-
-function addTodo() {
-  const todoText = todoInput.value.trim();
-  
-  // ตรวจสอบความยาวของข้อความ
-  if (todoText.length > 50) {
-    alert("Task must be less than 50 characters.");
-    return;
-  }
-
-  if (todoText.length > 0) {
-    const todo = {
-      id: Date.now(),
-      text: todoText,
-      completed: false,
-    };
-
-    todos.push(todo);
-    todoInput.value = "";
-    renderTodos();
-  }
+function changeToX(event) {
+  // Get the element that was clicked
+  const container = event.currentTarget;
+  // Create an <img> tag with the X img src
+  const image = document.createElement('img');
+  image.src = X_IMAGE_URL;
+  // Append that <img> tag to the element
+  container.appendChild(image);
+  container.removeEventListener('click', changeToX)
 }
 
-function deleteTodo(id) {
-  // แสดง confirm box ก่อนลบ
-  const confirmation = confirm("Are you sure you want to delete this task?");
-  if (confirmation) {
-    todos = todos.filter((todo) => todo.id !== id);
-    renderTodos();
-  }
+const boxes = document.querySelectorAll('#grid div');
+for (const box of boxes) {
+  box.addEventListener('click', changeToX);
 }
-
-function toggleCompleted(id) {
-  todos = todos.map((todo) => {
-    if (todo.id === id) {
-      todo.completed = !todo.completed;
-    }
-    return todo;
-  });
-  renderTodos();
-}
-
-function renderTodos() {
-  todoList.innerHTML = "";
-
-  todos.forEach((todo) => {
-    const todoItem = document.createElement("li");
-    const todoText = document.createElement("span");
-    const todoDeleteButton = document.createElement("button");
-    const myCheck = document.createElement("input");
-
-    myCheck.setAttribute("type", "checkbox");
-
-    todoText.textContent = todo.text;
-    todoDeleteButton.textContent = "Delete";
-
-    // ฟังก์ชันเช็คการทำงานเสร็จ
-    if (todo.completed) {
-      todoText.style.textDecoration = "line-through";
-      myCheck.checked = true;
-    }
-
-    todoDeleteButton.addEventListener("click", () => deleteTodo(todo.id));
-
-    todoItem.appendChild(myCheck);
-    todoItem.appendChild(todoText);
-    todoItem.appendChild(todoDeleteButton);
-
-    myCheck.addEventListener("click", () => toggleCompleted(todo.id));
-
-    todoList.appendChild(todoItem);
-  });
-}
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  addTodo();
-});
-
-renderTodos();
